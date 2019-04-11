a01e643ee9cf4cba47656ba1a0a50d30187bbd7a
Zhu Zhan Yan <program.nom@gmail.com>
2019-04-11 13:24:08 +0800

    added fit to model to fit model to data

--- src/dab/model.py
+++ src/dab/model.py
@@ -4,27 +4,61 @@
 # model.py
 #
 
+import math
 import numpy as np
 from data.dataset import Dataset
 from multiprocessing import Pool, cpu_count
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
     
 # Dabnet model
 class Model:
+    def __init__(self, scaler=None, model=None):
+        self.scaler = scaler if scaler else StandardScaler()
+    
+        # Create model if does not already present 
+        M = 40
+        if not model: model = RandomForestClassifier(n_estimators=M,
+                                                     max_features=math.floor(M ** 0.5),
+                                                     n_jobs=-1)
+        self.backend_model = model
+        
+    
+    # Preprocess the given inputs for machine learning
+    def preprocess(self, inputs):
+        scaled_inputs = self.scaler.transform(inputs)
+        return scaled_inputs
+         
+    
     # Fit the model to the given data given as inputs, expected outputs
-    def fit(self, inputs, outputs, split=0.3):
+    # Returns r2 score of model
+    def fit(self, inputs, outputs, split=0.3, verbose=True):
+        ## Preprocess data
         # Split dataset into test and train subsets
         train_inputs, test_inputs, train_outputs, test_outputs = \
             train_test_split(inputs, outputs, test_size=0.3, shuffle=True)
-        test = train_test_split(inputs, test_size=0.3)
+        if verbose: 
+            print("train on {}, test on {}".format(len(train_inputs), 
+                                                   len(test_inputs)))
     
-        
-    # Predict the outputs for the given
-    def predict(self, inputs): 
-        pass
+        # preprocess inputs
+        self.scaler.fit(train_inputs)
+        train_inputs = self.preprocess(train_inputs)
+        test_inputs = self.preprocess(test_inputs)
+    
+        # fit model to data
+        self.backend_model.fit(train_inputs, train_outputs)
+        if verbose:
+            print("train score:", self.backend_model.score(train_inputs, train_outputs))
+            print("test score:", self.backend_model.score(test_inputs, test_outputs))
+    
 
+    # Predict the outputs for the given
+    def predict(self, inputs):
+        pass
+    
 if __name__ == "__main__":
-    dataset = Dataset(n_limit=30)
+    dataset = Dataset()
     model = Model()
     model.fit(dataset.inputs, dataset.outputs)
