c8b5d3714471e3ab72a3d9301f176b074e67f603
Zhu Zhan Yan <program.nom@gmail.com>
2019-04-11 10:16:29 +0800

    Added dataset.prepare to prepare data for machine learning

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -8,9 +8,10 @@ import os
 import numpy as np
 import pandas as pd
 import cv2
-from pose.client import request_pose
-from data.preprocessing import encode_one_hot
+from data.preprocessing import encode_one_hot, extract_pose_features
 from multiprocessing import Pool, cpu_count
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # Dataset path constants
 PATH = "data"
@@ -18,17 +19,25 @@ IMG_PATH = os.path.join(PATH, "images")
 CACHE_PATH = os.path.join(PATH, "cache")
 META_PATH = os.path.join(PATH, "meta.csv")
 
-
 class Dataset:
-    def __init__(self):
-        self.images, self.labels, self.label_index = self.load()
+    # n_limit - limits dataset size
+    # split - test train split ratio
+    def __init__(self, n_limit=None, split=0.3):
+        self.images, self.labels, self.label_index = self.load(n_limit)
+        self.split_ratio = split
+        self.prepare()
+
         
     # Loads the dataset from disk
+    # limits dataset to  n_limit entries
     # Return images, labels, label_index
-    def load(self):
+    def load(self, n_limit):
         # read dataset metadata
         df = pd.read_csv(META_PATH, 
                          dtype={"img_path": str, "label": "category"})
+        df = df.sample(frac=1) # shuffle dataset
+        df = df.sample(n_limit)
+        # extract dataset laabels
         labels =  df.loc[:, "label"].cat.codes.values
         index = df.loc[:, "label"].cat.categories
         label_index = dict([ (label_idx, label_str) for label_idx, label_str in \
@@ -39,17 +48,16 @@ class Dataset:
         images = [ cv2.imread(p) for p in img_paths ]
         
         return images, labels, label_index
-    
+
+    # Prepare the dataset images and labels for machine learning
+    def prepare(self):
+        self.features = extract_pose_features(self.images)
+        self.labels = encode_one_hot(self.labels)
+        
     # Lookup human readable version of the given integer label
     # returns the human readable label
     def lookup_label(self, label_idx):
         return self.label_index[label_idx]
 
-    # Generate expected ouputs for machine learning
-    @property
-    def generate_outputs(self):
-        return encode_one_hot(self.labels)
-
 if __name__ == "__main__":
-    dataset =  Dataset()
-    print(dataset.lookup_label(0))
+    dataset =  Dataset(20)
--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import multiprocessing
 
+from pose.client import request_pose
 
 # One encode the given inputs
 def encode_one_hot(inputs):
@@ -16,3 +17,23 @@ def encode_one_hot(inputs):
     encoding[np.arange(len(inputs)), inputs] = 1
     
     return encoding
+
+# Flatten the np arrays in the given nested list of pose features
+def flatten_features(features):
+    # Flatten the numpy array feature
+    def flatten_inner(features):
+        return [ np.ravel(feat) for feat in features ]
+    features = [ flatten_inner(feat_set) for feat_set in features ]
+
+    # stack each featre in feature set together
+    features = [ np.concatenate(feat_set) for feat_set in features ]
+
+    return features
+
+# Extract flattened features for the given images
+# Returns extracted flattened fewatures
+def extract_pose_features(images):
+    pose_feats = [ request_pose(img) for img in images ]
+    flat_pose_feats = flatten_features(pose_feats)
+
+    return flat_pose_feats
